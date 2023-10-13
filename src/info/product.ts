b6c0617f0205352ad3cfab6822e5d76958a1042d
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-10-13 16:48:14 -0300

    Set script elements type attributes to module

---
+++ src/info/product.ts
@@ -0,0 +1,8 @@
+document.body.innerHTML = `
+  <h1>Product info</h1>
+  <h3>Super Computer</h3>
+  <p>The best computer for you!</p>
+  <input type="text" name="price" value="$399.99">
+  <button type="button">Buy</button>
+`;
+
---
+++ src/user/profile.ts
@@ -0,0 +1,11 @@
+document.body.innerHTML = `
+  <h1>User info</h1>
+  <h3>John Yellow</h3>
+  <p>Software developer, a great one!</p>
+  <p>Contact me</p>
+  <ul>
+    <li><a href="#">Gmail</a></li>
+    <li><a href="#">Linkedin</a></li>
+  </ul>
+`;
+
