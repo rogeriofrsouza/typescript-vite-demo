9fa96107cbc662c7f332f053b2b93b85462d7c7b
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-10-13 15:59:52 -0300

    Multi-Page App configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/typescript-vite-demo/'
+  base: '/typescript-vite-demo/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        info: resolve(__dirname, 'src/info/product.html'),
+        profile: resolve(__dirname, 'src/user/profile.html')
+      }
+    }
+  }
 });
